1d822c1c580057e40b43c7544dbeb9d92f0a2792
katerynapidhorna <katerynapidhorna@gmail.com>
2020-09-23 13:56:44 +0200

    union type

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 let sayHi = "Hello World";
 let myName = "Kate";
+// tsc -w  => watch the file changes and compile them
+//union type
+function totalLength(x, y) {
+    let total = x.length + y.length;
+    x.slice(0);
+    if (x instanceof Array) {
+        //type guard syntax
+        x.push("abc");
+    }
+    if (typeof x === "string") {
+        //typeof => because string is a primitive type
+        // and this syntax is only reserved for the couple the primitive types
+        //I can also use an "instanceof String"
+        x.substr(1);
+    }
+    return total;
+}
--- app.ts
+++ app.ts
@@ -1,2 +1,25 @@
 let sayHi: string = "Hello World";
 let myName: string = "Kate";
+
+// tsc -w  => watch the file changes and compile them
+
+//union type
+function totalLength(x: string | any[], y: string | any[]): number {
+  let total: number = x.length + y.length;
+
+  x.slice(0);
+
+  if (x instanceof Array) {
+    //type guard syntax
+    x.push("abc");
+  }
+
+  if (typeof x === "string") {
+    //typeof => because string is a primitive type
+    // and this syntax is only reserved for the couple the primitive types
+    //I can also use an "instanceof String"
+    x.substr(1);
+  }
+
+  return total;
+}
